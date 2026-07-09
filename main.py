1b8b6cba7c176fc92cadcb65d701550f7dc64ecf
CNS-Kairos <167649647+CNS-Kairos@users.noreply.github.com>
2026-07-09 18:30:55 -0400

    Se añadio main.py a raiz
    se integró main.py con placeholders para ejecutar la pipeline más adelante

---
+++ main.py
@@ -0,0 +1,41 @@
+# Para que encuentre 'src' y pueda importar los scripts de cada etapa
+import sys
+
+# Importar las funciones de las etapas del pipeline
+from src.ingesta import ejecutar_ingesta
+
+
+def main():
+    print("============================================================")
+    print(" INICIANDO PIPELINE DE DATOS: DATAMART CHILE S.A.")
+    print("============================================================\n")
+    
+    try:
+        # Etapa 1: Ingesta
+        print("[PIPELINE] Ejecutando Etapa 1: Ingesta de datos...")
+        df_raw = ejecutar_ingesta('ventas_datamart.csv')
+        print("[PIPELINE] Etapa 1 finalizada con éxito.\n")
+        
+
+        # Etapa 2: Limpieza y Transformación (Placeholder)
+        print("[PIPELINE] Etapa 2: Limpieza y Transformación -> ESPERANDO INTEGRACIÓN")
+        print("-" * 60 + "\n")
+        
+        # Etapa 3: Validación (Placeholder)
+        print("[PIPELINE] Etapa 3: Validación con Pandera -> ESPERANDO INTEGRACIÓN")
+        print("-" * 60 + "\n")
+        
+        # Etapa 4: Carga a Base de Datos (Placeholder)
+        print("[PIPELINE] Etapa 4: Carga a BD (SQLite) -> ESPERANDO INTEGRACIÓN")
+        print("-" * 60 + "\n")
+        
+        print("============================================================")
+        print(" PIPELINE EJECUTADO PARCIALMENTE (ETAPA 1 COMPLETADA)")
+        print("============================================================")
+
+    except Exception as e:
+        print(f"\n[PIPELINE ERROR] El flujo se detuvo debido a un fallo: {str(e)}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
